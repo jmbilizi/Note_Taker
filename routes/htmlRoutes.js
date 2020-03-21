04719ebade77dc89ff6aaf05fdf8b804d33c45bf
Admin <admin@Admins-MacBook-Pro.local>
2020-03-21 10:10:26 -0600

    deburg

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const path = require("path");
 
 //GET `/` - Should return the `index.html` file.
-router.get("/", (req, res) => {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 //GET `/notes` - Should return the `notes.html` file.
-router.get("/notes/", (req, res) => {
+router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
